70dd58f32c4fe3aeb979f6b323f51a3964c51cbe
Mohammad Alyounes <fcbmoh98@gmail.com>
2025-05-15 20:40:35 +0300

    Add and display metrics for the sales and remove data rows from display

--- dashboard.py
+++ dashboard.py
@@ -2,6 +2,7 @@ import streamlit as st
 from data_wrangling import prepare_data, get_filtered_data_within_date_range
 from filter_panel import filter_panel
 from date_range_panel import date_range_panel
+from metric_bar import metric_bar
 
 st.set_page_config(layout='wide')
 st.title("By Mohammad Alyounes")
@@ -14,4 +15,9 @@ data = prepare_data()
 filters = filter_panel(data)
 
 main_df = get_filtered_data_within_date_range(data, start, end, filters)
-st.write(main_df.head(5))
+
+if main_df.empty:
+    st.warning("No data to display")
+else:
+    metric_bar(main_df)
+
