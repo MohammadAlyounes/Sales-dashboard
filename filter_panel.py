d69e0c7714d4c21e4b09cdfbbd74e56075f6d92c
Mohammad Alyounes <fcbmoh98@gmail.com>
2025-05-17 16:35:33 +0300

    Make the interdependency between filters

--- filter_panel.py
+++ filter_panel.py
@@ -1,8 +1,9 @@
 import streamlit as st
-from data_wrangling import get_unique_values
+from data_wrangling import get_unique_values, apply_filters
 
 filter_dims = ["Age group", "Gender", "Category", "Segment",
 "Product name", "State"]
+
 
 def filter_panel(df):
     """takes a dataframe as input and renders the dropdowns
@@ -10,9 +11,11 @@ def filter_panel(df):
     filters = {}
     with st.expander("Filters"):
         filter_cols = st.columns(len(filter_dims))
+        effective_df = df
         for idx, dim in enumerate(filter_dims):
             with filter_cols[idx]:
-                unique_values = get_unique_values(df, dim)
+                effective_df = apply_filters(effective_df, filters)
+                unique_values = get_unique_values(effective_df, dim)
                 filters[dim] = st.multiselect(dim, unique_values)
     return filters
 
